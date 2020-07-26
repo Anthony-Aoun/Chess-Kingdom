7cba6ec7549f323cc9b479584c212fb30db3e2c3
AnthonyAoun7 <anthony.aoun2000@gmail.com>
2020-07-27 00:27:57 +0300

    backup

--- js/defs.js
+++ js/defs.js
@@ -1,11 +1,9 @@
 //define the class pieces
 class piece {
-    constructor(type, color, i, j) {
+    constructor(type, color) {
     //initial status of all pieces
         this.type = type
         this.color = color
-        this.i = i
-        this.j = j
         //all pieces default
         this.moved = false //king-rook-pawn
     }
@@ -16,7 +14,7 @@ class square {
     constructor(color) {
         this.color = color
         //square default
-        this.status = new piece("none", "none", -1, -1)
+        this.status = new piece("none", "none")
     }
 }
 
@@ -26,10 +24,7 @@ class game {
     this.board = new Array(8)
     this.saved_boards = []
     this.saved_moves = []
-    this.alive_white = []
-    this.alive_black = []
-    this.eaten_white = []
-    this.eaten_black = []
+    this.saved_eaten = []
     this.turn = "white"
     
     //transform board into a matrix of squares
@@ -44,35 +39,37 @@ class game {
     }
 
     //fill the pieces in the squares of the board
-    this.board[0][0].status = new piece("rook", "black", 0, 0)
-    this.board[0][1].status = new piece("knight", "black", 0, 1)
-    this.board[0][2].status = new piece("bishop", "black", 0, 2)
-    this.board[0][3].status = new piece("queen", "black", 0, 3)
-    this.board[0][4].status = new piece("king", "black", 0, 4)
-    this.board[0][5].status = new piece("bishop", "black", 0, 5)
-    this.board[0][6].status = new piece("knight", "black", 0, 6)
-    this.board[0][7].status = new piece("rook", "black", 0, 7)
+    this.board[0][0].status = new piece("rook", "black")
+    this.board[0][1].status = new piece("knight", "black")
+    this.board[0][2].status = new piece("bishop", "black")
+    this.board[0][3].status = new piece("queen", "black")
+    this.board[0][4].status = new piece("king", "black")
+    this.board[0][5].status = new piece("bishop", "black")
+    this.board[0][6].status = new piece("knight", "black")
+    this.board[0][7].status = new piece("rook", "black")
 
     for (let j=0; j<8; j++) {
-        this.board[1][j].status = new piece("pawn", "black", 1, j)
+        this.board[1][j].status = new piece("pawn", "black")
     }
 
     for (let j=0; j<8; j++) {
-        this.board[6][j].status = new piece("pawn", "white", 6, j)
+        this.board[6][j].status = new piece("pawn", "white")
     }
 
-    this.board[7][0].status = new piece("rook", "white", 7, 0)
-    this.board[7][1].status = new piece("knight", "white", 7, 1)
-    this.board[7][2].status = new piece("bishop", "white", 7, 2)
-    this.board[7][3].status = new piece("queen", "white", 7, 3)
-    this.board[7][4].status = new piece("king", "white", 7, 4)
-    this.board[7][5].status = new piece("bishop", "white", 7, 5)
-    this.board[7][6].status = new piece("knight", "white", 7, 6)
-    this.board[7][7].status = new piece("rook", "white", 7, 6)
+    this.board[7][0].status = new piece("rook", "white")
+    this.board[7][1].status = new piece("knight", "white")
+    this.board[7][2].status = new piece("bishop", "white")
+    this.board[7][3].status = new piece("queen", "white")
+    this.board[7][4].status = new piece("king", "white")
+    this.board[7][5].status = new piece("bishop", "white")
+    this.board[7][6].status = new piece("knight", "white")
+    this.board[7][7].status = new piece("rook", "white")
 
-    //start filling the saved_boards list with a clone of the first configuration
+    //start filling the saved_boards list with a clone of the first configuration and also the saved_eaten
     var boardClone = this.cloneBoard(this.board)
     this.saved_boards.push(boardClone)
+    var added_piece = new piece("none", "none")
+    this.saved_eaten.push(added_piece)
     }
 
     //clones a given board
@@ -91,7 +88,7 @@ class game {
         //fill the pieces
         for (let i=0; i<8; i++) {
             for (let j=0; j<8; j++) {
-                newBoard[i][j].status = new piece(board[i][j].status.type, board[i][j].status.color, board[i][j].status.i, board[i][j].status.j)
+                newBoard[i][j].status = new piece(board[i][j].status.type, board[i][j].status.color)
                 newBoard[i][j].status.moved = board[i][j].status.moved
             }
         }
@@ -140,8 +137,6 @@ class game {
             checking = this.isChecked(boardClone, saved_moves, turn)[1]
             boardClone[checking[0]][checking[1]].status.type = "none"
             boardClone[checking[0]][checking[1]].status.color = "none"
-            boardClone[checking[0]][checking[1]].status.i = -1
-            boardClone[checking[0]][checking[1]].status.j = -1
             search = this.isChecked(boardClone, saved_moves, turn)[0]
         }
         return nb
@@ -153,8 +148,6 @@ class game {
         var boardClone = this.cloneBoard(board)
         boardClone[posi[0]][posi[1]].status.type = "none"
         boardClone[posi[0]][posi[1]].status.color = "none"
-        boardClone[posi[0]][posi[1]].status.i = -1
-        boardClone[posi[0]][posi[1]].status.j = -1
         var after = this.nbChecked(boardClone, saved_moves, turn)
         if (after > before) {
             return true
@@ -431,7 +424,7 @@ class game {
                 return false
             }
             //left tower
-            else if (move[3] < move[1] && (board[move[0]][move[1]-1].status.type !== "none" || board[move[0]][move[1]-2].status.type !== "none" || board[move[0]][move[1]-3].status.type !== "none" || this.isThreatened(board, [move[0], move[1]-1], saved_moves, turn)[0] || this.isThreatened(board, [move[0], move[1]-2], saved_moves, turn)[0] || this.isThreatened(board, [move[0], move[1]-3], saved_moves, turn)[0])) {
+            else if (move[3] < move[1] && (board[move[0]][move[1]-1].status.type !== "none" || board[move[0]][move[1]-2].status.type !== "none" || board[move[0]][move[1]-3].status.type !== "none" || this.isThreatened(board, [move[0], move[1]-1], saved_moves, turn)[0] || this.isThreatened(board, [move[0], move[1]-2], saved_moves, turn)[0])) {
                 return false
             }
             //if the player passed all the tests he deserves to castle
@@ -442,16 +435,22 @@ class game {
         //check if it's a good move
         else if (!(this.goodMove(board, move, saved_moves, turn))) {
             return false
-        }
+        }    
         //is the start frozen?
-        else if (this.isFrozen(board,[move[0], move[1]], saved_moves, turn)) {
+        else if ( !(board[move[0]][move[1]].status.type === "king" && board[move[0]][move[1]].status.color === turn) && this.isFrozen(board,[move[0], move[1]], saved_moves, turn)) {
             return false
-        }
+        }    
         //if checked we can only move to an unchecked configuration
         else if (this.isChecked(board, saved_moves, turn)[0]) {
             //clone board, do the move and verify if the king is still threatened
             var boardClone = this.cloneBoard(board)
-            this.doMove(boardClone, move, saved_moves, turn)
+
+            var moving_piece = new piece(boardClone[move[0]][move[1]].status.type, boardClone[move[0]][move[1]].status.color)
+            moving_piece.moved = true
+            boardClone[move[2]][move[3]].status = moving_piece
+            boardClone[move[0]][move[1]].status.type = "none"
+            boardClone[move[0]][move[1]].status.color = "none"
+
             if (this.isChecked(boardClone, saved_moves, turn)[0]) {
                 return false
             }  
@@ -491,61 +490,108 @@ class game {
         return possible
     }
 
-    //mofifies the board after a move without asking if it's possible, just if it's good
-    doMove(board, move, saved_moves, turn) {
-
-        //////////////
-        ///  FILL  ///
-        //////////////
-
-        //don't check if the move is possible (check if it's goodMove)
-        //modify the board : type, color and position
-        //if king, rook or pawn moves modify piece.moved
-
-        //NB: if the move is castling be careful and if pawn on last line
-    }
-
     //makes the move for this.board knowing this.turn and modifies the global variables of the class
     //never use it with a cloned board because it modifies the global variables of game class
     playerMove(move) {
-
-        //////////////
-        ///  FILL  ///
-        //////////////
-
-        //check if the move is possible
-        //calls doMove()
-
-        //modify this.eaten_white
-        //modify this.eaten_black
-        //modify this.alive_white
-        //modify this.alive_black
-        //add it to saved_boards
-        //add move to saved_moves
-        //change this.turn
-        //search for the king and verify if isThreatened to modify this.check   
-        
-        //the above shouldn't be changed if the the move is not possible
-    }
-
-    //returns 0 if no one won, 1 if white won, 2 if black won
-    ////////////////////////////////////
-    ///A REVOIR et ajouter END GAME()///
-    ////////////////////////////////////
-    checkmate(board, saved_moves) {
-        if (!(this.check)) {
-            return 0
-        }
-        else {
-            if (this.possibleMoves(board, saved_moves, "black") === []) {
-                return 1
-            }
-            else if (this.possibleMoves(board, saved_moves, "white") === []) {
-                return 2
+        if (this.isPossible(this.board, move, this.saved_moves, this.turn)) {
+            //modify this.saved_eaten list
+            var added_piece = new piece("none", "none")
+            if (this.board[move[2]][move[3]].type === "none") {
+                this.saved_eaten.push(added_piece)
             }
             else {
-                return 0
+                added_piece.type = this.board[move[2]][move[3]].type
+                added_piece.color = this.board[move[2]][move[3]].color
+                this.saved_eaten.push(added_piece)
             }
+            //modify this.saved_moves
+            this.saved_moves.push(move)
+            //check if the move is casteling
+            if (this.board[move[0]][move[1]].status.type === "king" && move[3]-move[1] === 2) {
+                this.board[move[0]][5].status.type = "rook"
+                this.board[move[0]][5].status.color = this.turn
+                this.board[move[0]][5].status.moved = true
+                this.board[move[0]][7].status.type = "none"
+                this.board[move[0]][7].status.color = "none"
+            }
+            else if (this.board[move[0]][move[1]].status.type === "king" && move[1]-move[3] === 2) {
+                this.board[move[0]][3].status.type = "rook"
+                this.board[move[0]][3].status.color = this.turn
+                this.board[move[0]][3].status.moved = true
+                this.board[move[0]][0].status.type = "none"
+                this.board[move[0]][0].status.color = "none"
+            }
+            //check if the move is a pawn to last line
+            //////////////
+            ///  FILL  ///
+            //////////////
+            //modify this.board
+            var moving_piece = new piece(this.board[move[0]][move[1]].status.type, this.board[move[0]][move[1]].status.color)
+            moving_piece.moved = true
+            this.board[move[2]][move[3]].status = moving_piece
+            this.board[move[0]][move[1]].status.type = "none"
+            this.board[move[0]][move[1]].status.color = "none"
+            //modify this.saved_boards
+            var boardClone = this.cloneBoard(this.board)
+            this.saved_boards.push(boardClone)
+            //modify this.turn
+            if (this.turn === "white") {
+                this.turn = "black"
+            }
+            else {
+                this.turn = "white"
+            }
+            
+            //this.printBoard(this.board, this.turn)
+        }
+        
+    }
+
+    //see if 'color' is checkmated
+    checkmate(board, saved_moves, color) {
+        if (!(this.isChecked(board, saved_moves, color))) {
+            return false
+        }
+        if (this.possibleMoves(board, saved_moves, color) === []) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    //see if 'color' is stalemated
+    stalemate(board, saved_moves, color) {
+        if (this.isChecked(board, saved_moves, color)) {
+            return false
+        }
+        if (this.possibleMoves(board, saved_moves, color) === []) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    //if game is ended returns [winner, method of winning], if not return ['none', 'none']
+    endGame(board, saved_moves) {
+        //white wins?
+        if (this.checkmate(board, saved_moves, "black")) {
+            return ["white", "checkmate"]
+        }
+        else if (this.stalemate(board, saved_moves, "black")) {
+            return ["white", "stalemate"]
+        }
+        //black wins?
+        else if (this.checkmate(board, saved_moves, "white")) {
+            return ["black", "checkmate"]
+        }
+        else if (this.stalemate(board, saved_moves, "white")) {
+            return ["black", "stalemate"]
+        }
+        //no one wins
+        else {
+            return ["none", "none"]
         }
     }
 
@@ -623,12 +669,18 @@ class game {
         }
         //mention the turn  
         document.getElementById("player").innerHTML = turn
+
+        //////////////////////
+        ///ADD EATEN PIECES///
+        //////////////////////
+        //and add it as an argument
     }
 }
 
-
+/*
 //TEST
 myGame = new game()
+
 myGame.board[6][0].status.type="none"
 myGame.board[6][0].status.color="none"
 myGame.board[6][1].status.type="none"
@@ -653,8 +705,9 @@ myGame.board[7][6].status.color="none"
 
 myGame.printBoard(myGame.board, myGame.turn)
 
-function getInputValue() {
-    var testMove = document.getElementById("myInput").value
+
+function getTestValue() {
+    var testMove = document.getElementById("myTest").value
     var i = 8 - parseInt(testMove[1])
     var j = -1
     if (testMove[0] === 'A') {
@@ -716,12 +769,82 @@ function getInputValue() {
     }
 }
 
-//console.log(myGame.possibleMoves(myGame.board, myGame.saved_moves, myGame.turn))
-console.log(myGame.isPossible(myGame.board, [7,7,7,5], myGame.saved_moves, myGame.turn))
+function getMoveValue() {
+    var move = document.getElementById("myMove").value
+    var i = 8 - parseInt(move[1])
+    var j = -1
+    if (move[0] === 'A') {
+        j = 0
+    }
+    else if (move[0] == 'B') {
+        j = 1
+    }
+    else if (move[0] == 'C') {
+        j = 2
+    }
+    else if (move[0] == 'D') {
+        j = 3
+    }
+    else if (move[0] == 'E') {
+        j = 4
+    }
+    else if (move[0] == 'F') {
+        j = 5
+    }
+    else if (move[0] == 'G') {
+        j = 6
+    }
+    else if (move[0] == 'H') {
+        j = 7
+    }
+    var k = 8 - parseInt(move[3])
+    var l = -1
+    if (move[2] === 'A') {
+        l = 0
+    }
+    else if (move[2] == 'B') {
+        l = 1
+    }
+    else if (move[2] == 'C') {
+        l = 2
+    }
+    else if (move[2] == 'D') {
+        l = 3
+    }
+    else if (move[2] == 'E') {
+        l = 4
+    }
+    else if (move[2] == 'F') {
+        l = 5
+    }
+    else if (move[2] == 'G') {
+        l = 6
+    }
+    else if (move[2] == 'H') {
+        l = 7
+    }
 
-//test doMove by trying to go from a checked position to an unchecked one
-//game can also end if......cyril
+    if (myGame.isPossible(myGame.board, [i,j,k,l], myGame.saved_moves, myGame.turn)) {
+        myGame.playerMove([i,j,k,l])
+        
+    }
+    else {
+        console.log("COULDN'T MAKE MOVE. SOMETHING WENT WRONG :/")
+    }
+}
+
+
+console.log(myGame.possibleMoves(myGame.board, myGame.saved_moves, myGame.turn))
+*/
+
+//PRINTING BOARD PROBLEM!!!
+//scroll problem in online.html (or online.css)
+//check if the move is a pawn to last line in  playerMove
+//test by trying to go from a checked position to an unchecked one
+//game can also ends if : https://www.chess.com/article/view/how-chess-games-can-end-8-ways-explained
 //(when quiting add do you want to save the game)
 //UNDO (rounded arrow)
 //OPTION (clock settings) : save game, new game, win, resign, draw
-//EATEN pieces : reduce them to one list and remove alive_pieces and create saved_eaten_pieces
+//add EATEN pieces output 
+//add CHECK output next to turn 
+//add pop-up window YOU WON when game is over
---
+++ js/multiplayer.js
@@ -0,0 +1,2 @@
+var myGame = new game()
+
